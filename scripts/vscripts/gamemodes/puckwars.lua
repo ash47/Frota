795e69ee742a22d1ccb9b90a8e531d180c4550d2
Aedan Dispenza <aedan.dispenza@gmail.com>
2014-01-11 04:39:04 -0500

    puck wars!

---
+++ scripts/vscripts/gamemodes/puckwars.lua
@@ -0,0 +1,60 @@
+-- Invoker Wars
+RegisterGamemode('puckwars', {
+    -- Gamemode only has a gameplay component
+    sort = GAMEMODE_BOTH,
+
+    -- Function to give out heroes
+    assignHero = function(frota, ply)
+        -- Change heroes
+        ply:ReplaceHeroWith('npc_dota_hero_puck', 2500, 2600)
+
+        local playerID = ply:GetPlayerID()
+        local hero = Players:GetSelectedHeroEntity(playerID)
+
+        -- Apply the build
+        -- frota:LoadBuildFromHero(hero)
+    end,
+
+    -- A list of options for fast gameplay stuff
+    options = {
+        -- Kills give team points
+        killsScore = true,
+
+        -- Enable scores
+        useScores = true,
+
+        -- Respawn delay
+        respawnDelay = 3
+    },
+
+    voteOptions = {
+        -- Score limit vote
+        scoreLimit = {
+            -- Range based
+            s = VOTE_SORT_RANGE,
+
+            -- Minimal possible value
+            min = 1,
+
+            -- Maximal possible value
+            max = 50,
+
+            -- Default vaule (if no one votes)
+            def = 10,
+
+            -- Slider tick interval
+            tick = 10,
+
+            -- Slider step interval
+            step = 1
+        }
+    },
+
+    onGameStart = function(frota)
+        -- Grab options
+        local options = frota:GetOptions()
+
+        -- Set the score limit
+        frota:SetScoreLimit(options.scoreLimit)
+    end
+})
