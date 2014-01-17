83b85c125412c44fb945ce48ffd2de38d4c62784
hanni7345 <hanni7345@gmail.com>
2014-01-17 19:10:37 +0100

    added oddball item
    added oddball item and balance to mid creep

--- scripts/vscripts/gamemodes/oddball.lua
+++ scripts/vscripts/gamemodes/oddball.lua
@@ -43,8 +43,9 @@ RegisterGamemode('oddball', {
 			callback = function(frota, args)    
 				local vector = Vec3( 0, 0, 0 )
 				local unit = CreateUnitByName("npc_dota_neutral_satyr_trickster2", vector, false, nil, nil, DOTA_TEAM_NEUTRALS)
-				unit:AddItem(CreateItem('item_gem', nil, nil))    			
-				unit:SetMaxHealth(5000)
+				unit:AddItem(CreateItem('item_oddball', nil, nil))    			
+				cply = frota:GetPlayerList()
+				unit:SetMaxHealth(#cply * 500)
 				unit:SetHealth(5000)
 			end   
 		})
@@ -56,7 +57,7 @@ RegisterGamemode('oddball', {
             for i=0, 5 do
                 local item = hero:GetItemInSlot(i)
                 if item then
-                    if item:GetAbilityName() == 'item_gem' then
+                    if item:GetAbilityName() == 'item_oddball' then
 						hero_has_gem=hero
 						any_gem_active = 1
                     end
@@ -66,12 +67,32 @@ RegisterGamemode('oddball', {
     end,
 		
 	onThink = function(frota, dt)
+			--[[if frota.scoreRadiant % 500 == 0 then
+		local playerID = frota:ply:GetPlayerID()
+		local hero = frota:GetActiveHero(playerID)
+			if  hero:GetTeam() == DOTA_TEAM_BADGUYS then
+				local cgold = GetGold()
+				hero:SetGold(cgold+250, true)
+				frota.scoreRadiant = frota.scoreRadiant + 1
+				frota:UpdateScoreData()
+			end
+		end
+		if frota.scoreDire % 500 == 0 then
+	    local playerID = frota.ply:GetPlayerID()
+		local hero = frota:GetActiveHero(playerID)
+			if  hero:GetTeam() == DOTA_TEAM_GOODGUYS then
+				local cgold = GetGold()
+				hero:SetGold(cgold+250, true)
+				frota.scoreDire = frota.scoreDire + 1
+				frota:UpdateScoreData()
+			end
+		end]]
 		if any_gem_active == 1 then
 		hero = hero_has_gem
 			for i=0,5 do
 				local item = hero:GetItemInSlot(i)
 				if item then
-					if item:GetAbilityName() == 'item_gem' then
+					if item:GetAbilityName() == 'item_oddball' then
 						if  hero:GetTeam() == DOTA_TEAM_GOODGUYS then
 							frota.scoreRadiant = frota.scoreRadiant + 1
 							frota:UpdateScoreData()
