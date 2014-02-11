c5870aab92793afade9aae4b4603ea654d6bdb10
XavierCHN <364671459@qq.com>
2014-02-11 11:35:39 +0800

    add FOM downlimit

--- scripts/vscripts/addons/fatometer.lua
+++ scripts/vscripts/addons/fatometer.lua
@@ -26,10 +26,15 @@ RegisterGamemode('fatometer', {
                 -- Lose 1/3 of the size
                 scale[playerID] = (scale[playerID] or frota:GetDefaultHeroScale(killedUnit:GetClassname())) * 2/3
 
-                -- But don't be too small
-                if scale[playerID] < 0.5 then
-                    scale[playerID] = 0.5
-                end
+					-- Limit scale to 3
+ +                	local limit = frota:GetDefaultHeroScale(hero:GetClassname()) * 3
+ +                	local downlimit = frota:GetDefaultHeroScale(hero:GetClassname()) * 0.5
+ +                	if scale[playerID] > limit then
+ +                    	scale[playerID] = limit
+ +                	end
+					if scale[playerID] < downlimit then
+ +                    	scale[playerID] = downlimit
+ +                	end
 
                 -- Scale the hero
                 killedUnit:SetModelScale(scale[playerID], 0)
